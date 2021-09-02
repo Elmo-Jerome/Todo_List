93c7ea06067c0c7307591cb40c1b75eab35f6aef
Elmo Jerome <elmojeromem@gmail.com>
2021-09-02 23:12:56 +0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          {/* division for tasks */}
+          <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +22,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {},
 });
---
+++ components/Task.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Task = (props) => {
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+            <View style={styles.circular}></View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#FFF',
+    },
+    itemLeft: {},
+    square: {},
+    itemText: {},
+    circular: {},
+
+});
+
+export default Task;
